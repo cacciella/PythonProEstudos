5de1dd48015ad4fce6ea3e3e6f7e7ec5bf772e71
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2020-03-23 11:56:08 -0300

    Create Aula_Pro_02.py

---
+++ Aula_Pro_02.py
@@ -0,0 +1,19 @@
+# Operações aritméticas
+
+# soma + subtração - multiplicação * e divisão /
+# potencia **  divisão inteira //  resto da divisão %  raiz quadrada n**(1/2) raiz ao cubo n**(1/3)
+
+# ordem de precedencia:  1o () , 2o **,  3o * ,/, //, % ,  4o  + e -
+
+print('-=' * 40)
+
+nome = str( input('Qual é o seu nome? '))
+print('prazer em te conhecer {}'.format(nome))
+print('prazer em te conhecer {:>20}'.format(nome))
+print('prazer em te conhecer {:<20}'.format(nome))
+print('prazer em te conhecer {:^20}'.format(nome))
+print('prazer em te conhecer {:=^20}'.format(nome))
+
+print(pow(4,2))
+
+print(4 ** 2)
