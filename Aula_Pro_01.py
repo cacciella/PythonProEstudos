43f5fa0bddc9dc6df0823065d613ad57a9389efc
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2020-03-23 12:15:06 -0300

    Update Aula_Pro_01.py

--- Aula_Pro_01.py
+++ Aula_Pro_01.py
@@ -1,11 +1,17 @@
-# basico
+# Tipo Inteiro
+# Tipo Float
+# Variável e Atribuição
+# Tipo Booleano
+# Desvios Condicionais
 
 print(type(13))
+print(type(1.34))
+print(type('nome'))
+print(type(True))
+print(' - =' *10)
 print((2+2)-1)
 print(2*2)
-
 print(5/2)
 print(type(5/2))
-
 # exponencial
 print(2**3)
