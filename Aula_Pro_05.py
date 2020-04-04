04ddbb8053397ae21e13e598f9892a9966133037
VAIO-MC3 <arc.mau@gmail.com>
2020-04-04 18:35:15 -0300

    Desvios Condicionais

--- Aula_Pro_05.py
+++ Aula_Pro_05.py
@@ -2,13 +2,16 @@
 
 print('Python \nPro')
 print('Python' + ' Pro')
-print(' Ipsum Lorem  ***' * 20)
+print(' Ipsum Lorem  *** \n' * 10)
 
 # apresenta todos os atributos
 print(' =-  '* 15)
 print(dir(''))
+print()
+
 print(' ----- Informações sobre os atributos existentes dos componentes de objetos  -----  ')
 print(help('Mauricio'.lower()))
 
+print()
 print(('Mauricio'.lower()))
 print(('Mauricio'.upper()))
