86d81ca8dcc810bc84c2afc9dbb6068aa4326803
VAIO-MC3 <arc.mau@gmail.com>
2020-04-13 17:56:47 -0300

    rascunho - Dicionário e Lista

---
+++ rascunho_03.py
@@ -0,0 +1,31 @@
+# Contagem de Caracteres com Dicionário
+print()
+
+def contar_carter(s):
+    resultado = {}
+    for caracter in s:
+        resultado[caracter] = resultado.get(caracter, 0) + 1
+    return resultado
+
+if __name__ == '__main__':
+    print(contar_carter('Nooovinho'))
+    print()
+
+
+
+print()
+print()
+# Contagem de Caracteres com Lista
+
+n = 'Nome'
+for i in range(len(n)):
+    print(f'{i} : {n[i]}')
+
+print()
+print()
+
+nome3 = 'Novinho'
+for i in range(len(nome3)):
+    print(f'{i} recebe {nome3[i]}')
+
+
