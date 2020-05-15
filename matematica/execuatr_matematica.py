0c2078612fd1f7b7e9fbc1d7eec9c34205564cd4
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2020-05-15 15:25:07 -0300

    Update execuatr_matematica.py

--- matematica/execuatr_matematica.py
+++ matematica/execuatr_matematica.py
@@ -8,16 +8,12 @@ print(' -' *15)
 from matematica import mat
 print(mat.soma(4,5))
 
-
 print(' -' *15)
 #       -----     ou outra opção   ----
 from matematica.mat import soma
 print(soma(4,5))
 
-
 print(' -' *15)
 #       -----     ou outra opção  usando um apelido    as "s"   para abreviar  ----
 from matematica.mat import soma as s
 print(s(4,5))
-
-
