bd943c490fcf18db5464c5dadbb4fc8e4344812b
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2023-03-03 16:19:02 -0300

    Update Aula_Pro_04.py

--- Aula_Pro_04.py
+++ Aula_Pro_04.py
@@ -1,11 +1,11 @@
 #  Desvios Condicionais
 
-idade = int(input('Qual sua idade? '))
-if idade <= 15:
+idade = int(input('Qual sua idade?  '))
+if idade <= 16:
     print('Vc não pode ir ao Cinema')
 elif idade >= 60:
-    print('Acho....que vc não vai gostar deste filme ')
+    print('Acho que vc não vai gostar deste filme ')
 else:
-    print('Certo........ pode ir ao Cinema')
+    print('Certo, está lberado ........ pode ir ao Cinema')
 
 print(dir('\n'))
